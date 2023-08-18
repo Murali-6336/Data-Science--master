78c1083d8cd961c2820b56e1a64a975d0312b12f
Murali-6336 <muraleeswaranmca@gmail.com>
2023-08-18 18:51:12 +0530

    Files uploaded

---
+++ 09-Software-Engineering-and-Reproducible-Research/README.md
@@ -0,0 +1,13 @@
+# Software Engineering and Reproducible Research
+Software Engineering and Reproducible Research for Data Science
+
+## The World Beyond Notebooks
+
+Python Notebooks are great - they let us explore data and communicate and share results. But if you want to write more general-purpose reusable code, you should put it in a *package* - like numpy, pandas, and the other great tools we depend on.
+
+A full production-grade library is a large undertaking, but this week we will build our own modest but still useful package with utility functions for common data science tasks.
+
+I decided to spend this week building out the following packages:
+
+https://github.com/chrisluedtke/clued
+https://github.com/chrisluedtke/divvy-data
---
+++ 18-CS-Algorithms/01-Sorting-and-Searching/src/iterative_sorting/iterative_sorting.py
@@ -0,0 +1,27 @@
+
+def selection_sort( arr ):  # O(n ^ 2)
+    max_i = len(arr)
+    for current_i in range(max_i - 1): min_val_i = current_i
+# find next smallest element
+    for i in range(current_i, max_i): 
+        if arr[i] < arr[min_val_i]: min_val_i = i arr[current_i], arr[min_val_i] = arr[min_val_i], arr[current_i]  # swap
+
+    return arr
+
+
+def bubble_sort(arr):  # O(n ^ 2)
+    swap_made = True
+    while swap_made:
+        swap_made = False
+        for i in range(len(arr) - 1):
+            # if next item less than current
+            if arr[i+1] < arr[i]:  
+                arr[i], arr[i+1] = arr[i+1], arr[i]  # swap
+                swap_made = True
+
+    return arr
+
+
+def count_sort( arr, maximum=-1 ):
+
+    return arr
